a440b1966d095685ef126aedc969a6b220de3819
Abubakr Saddiq <abubakrsaddickq@gmail.com>
2024-10-28 10:46:02 +0000

    middlewares and helper functions

---
+++ server/middlewares/auth.js
@@ -0,0 +1,17 @@
+import { SECRET } from "../config/index.js";
+import jwt from "jsonwebtoken";
+
+export const verifyToken = async (req, res, next) => {
+  const { token } = req.cookies;
+  if (!token) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  try {
+    const user = await jwt.verify(token, SECRET);
+    req.userId = user.id;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+};
