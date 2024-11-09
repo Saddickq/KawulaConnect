89baa37ff827f3328c5c9eb2a3f41531cc9e46ba
Abubakr Saddiq <abubakrsaddickq@gmail.com>
2024-11-09 16:57:09 +0000

    Deployment bottle necks

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:5173", "https://kawulaconnect.netlify.app/"],
+    origin: ["http://localhost:5173", "https://kawulaconnect.netlify.app"],
   })
 );
 
